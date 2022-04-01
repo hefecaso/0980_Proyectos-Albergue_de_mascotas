06b1bd8df2b8819baf73c7522618ef249e555a91
hefecaso <hfcarrerasoto@ieee.org>
2022-03-31 18:04:57 -0600

    reiniciando proyecto prro

--- Albergue_mascotas/Albergue_mascotas/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-#def hola_mundo(request): #Primera vista
-    #return HttpResponse("Hola mundo!")
-
-def index(request):
-    template_name = "index.html"
-    return render(request,template_name)
--- venv_refugio/refugio_canino/refugio/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RefugioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'refugio'
