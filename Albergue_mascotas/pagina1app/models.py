aebbc749594042fe5871683b001f7312d8e13e6d
hefecaso <hfcarrerasoto@ieee.org>
2022-04-10 20:35:21 -0600

    migrando contactos del modelo.py y admin.py a postgresql

--- Albergue_mascotas/pagina1app/models.py
+++ Albergue_mascotas/pagina1app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+#####################################
+#   Creando sección de consultas    #
+#####################################
+
+opciones_consultas=[
+    [0, "consulta"],
+    [1, "reclamo"],
+    [2, "sugerencia"],
+    [3, "felicitaciones"],
+]
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    tipo_consulta = models.IntegerField(choices=opciones_consultas)
+    mensaje = models.TextField()
+    avisos = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+        #Por cada clase agregada, hay que migrar
+        #python manage.py makemigrations
+        #python manage.py migrate
