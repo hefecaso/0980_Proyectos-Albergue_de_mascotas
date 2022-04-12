7d8e8a137ed6e90b495f9b3801d250d35b670639
hefecaso <hfcarrerasoto@ieee.org>
2022-04-12 01:32:17 -0600

    Contacto terminado y urls corregidas, checkpoint

--- Albergue_mascotas/pagina1app/urls.py
+++ Albergue_mascotas/pagina1app/urls.py
@@ -7,12 +7,12 @@ from pagina1app import views
 urlpatterns = [
 
     path('',views.home, name="Home"),
-    path('servicios',views.servicios, name="Servicios"),
-    path('albergue',views.albergue, name="Albergue"),
+    path('servicios/',views.servicios, name="Servicios"),
+    path('albergue/',views.albergue, name="Albergue"),
     #path('iniciosesion',views.iniciosesion, name="Inicio de sesión"),
-    path('contacto',views.contacto, name="Contacto"),
-    path('registro_y_adopcion',views.registro_y_adopcion, name="Formularios registro y adopcion"),
-    path('formulario_registro_mascota',views.formulario_registro_mascota, name="Formulario registro"),
-    path('formulario_adopcion',views.formulario_adopcion, name="Formulario adopcion"),
+    path('contacto/',views.contacto, name="Contacto"),
+    path('registro_y_adopcion/',views.registro_y_adopcion, name="Formularios registro y adopcion"),
+    path('formulario_registro_mascota/',views.formulario_registro_mascota, name="Formulario registro"),
+    path('formulario_adopcion/',views.formulario_adopcion, name="Formulario adopcion"),
     path('registro/',views.registro, name="Formulario Registro"),
 ]
