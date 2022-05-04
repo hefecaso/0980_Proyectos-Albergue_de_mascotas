636582fbb1224e0c4e369f064aab02d55b15de41
hefecaso <hfcarrerasoto@ieee.org>
2022-05-04 02:14:04 -0600

    Proceso de modificación docker-compose y actualizando controles docker

--- Albergue_mascotas/Docker_control.py
+++ Albergue_mascotas/Docker_control.py
@@ -11,7 +11,7 @@ def menu():
     print('======================\n')
 
     print("\n1. Ver repositorios locales.")
-    print("2. Ver contenedores activos y desactivados.")
+    print("2. Ver contenedores activos y desactivos.")
     print("3. Ver último contenedor creado.")
     print("4. Borrrar contenedor con id de imágen.")
     print("5. Construir contenedor de Django.")
@@ -22,7 +22,8 @@ def menu():
     print("10. Makemigrations docker-compose -> Django.")
     print("11. Migrate docker-compose -> Django.")
     print("12. Collectstatic docker-compose -> Django.")
-    print("13. Salir.\n")
+    print("13. Ingresar a contenedor.")
+    print("14. Salir.\n")
 
 
 while True:
@@ -31,100 +32,118 @@ while True:
     os.system ("clear")
 
     if opc == '1':
-        print('=====================================')
+        print('=================================================================================')
         print("Mostrando lista de repositorios")
         print("\nsudo docker image ls\n")
         system(f"sudo docker image ls")
-        print('=====================================')
+        print('=================================================================================')
 
 
     elif opc == '2':
-        print('====================================================================')
+        print('=================================================================================')
         print("Mostrando contenedores activos y desactivos")
         print("\nsudo docker ps -a\n")
         system(f"sudo docker ps -a")
-        print('====================================================================')
+        print('=================================================================================')
 
 
     elif opc == '3':
-        print('====================================================================')
+        print('=================================================================================')
         print("Mostrando último contenedor creado")
         print("\nsudo docker ps -l\n")
         system(f"sudo docker ps -l")
-        print('====================================================================')
+        print('=================================================================================')
 
 
     elif opc == '4':
-        print('====================================================================')
+        print('=================================================================================')
         print("Borrando contenedor con id de imágen")
         image = input("\nIngrese ID de imágen: ")
         print(f"\nsudo docker rmi -f {image}\n")
         system(f"sudo docker rmi -f {image}")
-        print('====================================================================')
+        print('=================================================================================')
 
 
     elif opc == '5':
-        print('====================================================================')
+        print('=================================================================================')
         print("Construyendo contenedor de Django\n")
         nombre = input("Ingrese el nombre para su contenedor: ")
         tag = input("Ingrese el nombre del tag: ")
         print(f'\nsudo docker build -t "{nombre}:{tag}" .\n')
         system(f'sudo docker build -t "{nombre}:{tag}" .')
-        print('====================================================================')
+        print('=================================================================================')
 
 
     elif opc == '6':
-        print('====================================================================')
+        print('=================================================================================')
         print("Publicando contenedor de Django\n")
         nombre = input("Ingrese el nombre para su contenedor: ")
         print(f"\nsudo docker run --publish 8000:8000 {nombre}\n")
         system(f"sudo docker run --publish 8000:8000 {nombre}")
-        print('====================================================================')
+        print('=================================================================================')
 
 
     elif opc == '7':
-        print('====================================================================')
+        print('=================================================================================')
         print("Borrando contenedor con id de contenedor\n")
         contenedor = input("Ingrese ID del contenedor: ")
         print(f"\nsudo docker rm -f {contenedor}\n")
         system(f"sudo docker rm -f {contenedor}")
-        print('====================================================================')
+        print('=================================================================================')
 
 
     elif opc == '8':
-        print('====================================================================')
+        print('=================================================================================')
         print("Subiendo con docker composer\n")
         print(f"\nsudo COMPOSE_HTTP_TIMEOUT=200 docker-compose up --build\n")
         system(f"sudo COMPOSE_HTTP_TIMEOUT=200 docker-compose up --build")
-        print('====================================================================')
+        print('=================================================================================')
 
     elif opc == '9':
-        print('====================================================================')
+        print('=================================================================================')
         print("Corriendo con docker composer\n")
         print(f"\nsudo COMPOSE_HTTP_TIMEOUT=200 docker-compose up\n")
         system(f"sudo COMPOSE_HTTP_TIMEOUT=200 docker-compose up")
-        print('====================================================================')
+        print('=================================================================================')
 
     elif opc == '10':
-        print('====================================================================')
+        print('=================================================================================')
         print("Migrate docker-compose -> Django\n")
         print(f"\nsudo COMPOSE_HTTP_TIMEOUT=200 docker-compose run django_app python manage.py makemigrations\n")
         system(f"sudo COMPOSE_HTTP_TIMEOUT=200 docker-compose run django_app python manage.py makemigrations")
-        print('====================================================================')
+        print('=================================================================================')
 
     elif opc == '11':
-        print('====================================================================')
+        print('=================================================================================')
         print("Migrate docker-compose -> Django\n")
         print(f"\nsudo COMPOSE_HTTP_TIMEOUT=200 docker-compose run django_app python manage.py migrate\n")
         system(f"sudo COMPOSE_HTTP_TIMEOUT=200 docker-compose run django_app python manage.py migrate")
-        print('====================================================================')
+        print('=================================================================================')
 
     elif opc == '12':
-        print('====================================================================')
+        print('=================================================================================')
         print("Collectstatic docker-compose -> Django\n")
         print(f"\nsudo COMPOSE_HTTP_TIMEOUT=200 docker-compose run django_app python manage.py collectstatic\n")
         system(f"sudo COMPOSE_HTTP_TIMEOUT=200 docker-compose run django_app python manage.py collectstatic")
-        print('====================================================================')
+        print('=================================================================================')
+
+    elif opc == '13':
+        print('=================================================================================')
+        print("Borrando contenedor con id de contenedor\n")
+        contenedor = input("Ingrese ID del contenedor: ")
+        print(f"\nsudo docker exec -it {contenedor} psql -U postgres\n")
+        system(f"sudo docker exec -it {contenedor} psql -U postgres")
+        print('=================================================================================')
+
+
+    elif opc == '14':
+        print('===================')
+        print("||   Saliendo.   ||")
+        print('===================')
+        break
+
 
     else:
-        break
+        menu()
+        opc = input("Ingrese una opción: ")
+        os.system ("clear")
